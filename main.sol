1d4382da03e6844d7fb7c67d2f391205e385a599
ShuuNP <91449806+ShuuNP@users.noreply.github.com>
2024-07-02 13:39:46 +0800

    Update main.sol

--- main.sol
+++ main.sol
@@ -2,15 +2,12 @@
 pragma solidity >=0.6.12 <0.9.0;
 
 contract MyToken {
-    // Public variables to store token details
     string public tokenName = "Sample";
     string public tokenAbbrv = "SMP";
     uint public totalSupply = 0;
 
-    // Mapping to store balances
     mapping(address => uint) public balances;
 
-    // Mint function to create tokens
     function mint(address _address, uint _value) public {
         require(_address != address(0), "Invalid address");
         require(_value > 50, "Value must be greater than 50");
@@ -18,10 +15,8 @@ contract MyToken {
         balances[_address] += _value;
     }
 
-    // Custom error definition for revert()
     error InsufficientBalance(string);
 
-    // Combined burn function with require and custom revert
     function burn(address _address, uint _value) public {
         require(_address != address(0), "Invalid address");
         require(_value >= 50, "Value must be greater or equal than 50");
